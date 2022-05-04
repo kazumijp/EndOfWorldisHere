54f55e2e569c0048eb95ee66b5e82b19f7c0611b
kazumijp <kazumidrg@gmail.com>
2022-05-04 20:12:08 -0300

    Update

---
+++ First Fundamentals/script.js
@@ -0,0 +1,15 @@
+let js ='amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("kZm");
+console.log(23);
+
+let firstName = "Matilda";
+let first = 'jonas'
+let firstNamePerson
+
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
